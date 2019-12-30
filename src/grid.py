03c802d2f671ffd75f8e4d6effb5b9fba6c26ba4
DarioCaptain <daapplab@gmail.com>
2019-12-30 15:48:23 +0100

    Added rule1 prototype

--- src/grid.py
+++ src/grid.py
@@ -1,4 +1,4 @@
-
+from copy import deepcopy
 
 class Grid():
     """
@@ -11,6 +11,36 @@ class Grid():
 
     def __init__(self):
         self.defaultSize = 32
+        self.currentTime = 0
         self.grid = [[0 for j in range(self.defaultSize)] for i in range(self.defaultSize)]
 
         #self.grid[13][5] = 1
+
+    def applyRules(self):
+        """
+        Apply the rules to all cells.
+        """
+
+        #copy current state
+        tmpGrid = deepcopy(self.grid)
+
+        #iterate ove all cells
+        for cX, row in enumerate(self.grid):
+            for cY, cell in enumerate(row):
+                
+                #apply the rules
+                self.__rule1(cX, cY, cell)
+
+        self.currentTime += 1
+
+
+    def __rule1(self, posX, posY, cell):
+        """
+        A dead cell with exactly three living neighbors has to be reborn.
+        """
+
+        #the cell needs to be dead
+        if(cell == 0):
+            #exactly three neighbors have to be alive
+            if():
+                cell = 1
