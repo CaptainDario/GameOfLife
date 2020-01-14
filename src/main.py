ba4bbee0c33d87b4bc7d4ee739232a74f5669daf
DarioCaptain <daapplab@gmail.com>
2020-01-14 21:17:26 +0100

    bug fixes and added moving the map
    * you cannot zoom in until there is no map anymore:
        * added zoom-in/out limits
    * restructured the code 
    * the map can now be moved

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import pygame
 from defaults import Defaults
 from grid import Grid
 from drawUtil import DrawUtil
+from camera import Camera
 
 
 pygame.init()
@@ -15,7 +16,12 @@ pygame.display.set_caption(Defaults.title)
  
 # Loop until the user clicks the close button.
 done, handled = False, False
- 
+
+#is the player Moving
+isMoving = False
+#the mouse position when the player started moving 
+relMousePos = (0, 0)
+
 #if the simulation is running
 isRunning = False
 
@@ -28,30 +34,31 @@ passedTime = 0
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-#instantiate grid
-grid = Grid()
+#instantiate grid and camera
+grid, camera = Grid(), Camera(Defaults.wWidth, Defaults.wHeight)
+
 
 
 
  
 # -------- Main Program Loop -----------
 while not done:
-    # --- Main event loop
+    # --- Main event loop (HANDLE USER INPUT)
     for event in pygame.event.get():
         #check if wheel is scrolled
         if event.type == pygame.MOUSEBUTTONDOWN:
             #zoom out
             if event.button == 4:
                 #only until certain zoom factor
-                if Defaults.cellHeight < Defaults.zoomOutLimit and \
-                   Defaults.cellWidth < Defaults.zoomOutLimit:
+                if Defaults.cellHeight < camera.zoomOutLimit and \
+                   Defaults.cellWidth < camera.zoomOutLimit:
                     Defaults.cellHeight += 1
                     Defaults.cellWidth += 1
             #zoom in
             if event.button == 5:
                 #only if it is not to small
-                if Defaults.cellHeight > Defaults.zoomInLimit and \
-                   Defaults.cellWidth > Defaults.zoomInLimit:
+                if Defaults.cellHeight > camera.zoomInLimit and \
+                   Defaults.cellWidth > camera.zoomInLimit:
                     Defaults.cellHeight -= 1
                     Defaults.cellWidth -= 1
         if event.type == pygame.QUIT:
@@ -61,32 +68,31 @@ while not done:
     # --- Game logic should go here
 
     #HANDLE USER INPUT
+    #MOVING THE BOARD
+    #print("camera position:", relMousePos[0] - pygame.mouse.get_pos()[0], relMousePos[1] - pygame.mouse.get_pos()[1])
+    if pygame.mouse.get_pressed()[0] == True and pygame.mouse.get_pressed()[2] == True and isMoving == False:
+        relMousePos = pygame.mouse.get_pos()
+        isMoving = True
+    if pygame.mouse.get_pressed()[0] == False or pygame.mouse.get_pressed()[2] == False:
+        isMoving = False
+    if isMoving:
+        newCamPos = (pygame.mouse.get_pos()[0] - relMousePos[0],
+                    pygame.mouse.get_pos()[1] - relMousePos[1])
+        camera.setPos(newCamPos)
+    
     #INITIALIZE BOARD
     if(grid.currentTime == 0):
-        #add alive cell
-        if(pygame.mouse.get_pressed()[0] == True):
-            if(pygame.mouse.get_pos()[1] < Defaults.gridSize):
-                grid.grid[pygame.mouse.get_pos()[0] // Defaults.cellHeight]\
-                        [pygame.mouse.get_pos()[1] // Defaults.cellWidth] = 1
-        #remove alive cell
-        if(pygame.mouse.get_pressed()[2] == True):
-            if(pygame.mouse.get_pos()[1] < Defaults.gridSize):
-                grid.grid[pygame.mouse.get_pos()[0] // Defaults.cellHeight]\
-                        [pygame.mouse.get_pos()[1] // Defaults.cellWidth] = 0
-
-    #MOUSE CONTROL
-    #Do one simulation step when left clicked
-    if(grid.currentTime > 0):
-        #do one simulation step (ASSIGN THIS TO THE ONE STEP BUTTON)
-        if(pygame.mouse.get_pressed()[0] == True and not handled and True == False):
-            grid.applyRules()
-
-    #update the game accordingly to the set speed
-    if(isRunning):
-        if(simulationSpeed - passedTime <= 0):
-            grid.applyRules()
-            passedTime = 0
-
+        #check that the cursor is not out of range
+        if(0 <= pygame.mouse.get_pos()[1] - camera.pos[1] < Defaults.gridSize and \
+           0 <= pygame.mouse.get_pos()[0] - camera.pos[0] < Defaults.gridSize):
+            #add alive cell
+            if(pygame.mouse.get_pressed()[0] == True and pygame.mouse.get_pressed()[1] == False):
+                    grid.grid[(pygame.mouse.get_pos()[0] - camera.pos[0]) // Defaults.cellHeight] \
+                            [(pygame.mouse.get_pos()[1] - camera.pos[1]) // Defaults.cellWidth] = 1
+            #remove alive cell
+            if(pygame.mouse.get_pressed()[2] == True and pygame.mouse.get_pressed()[0] == False):
+                    grid.grid[(pygame.mouse.get_pos()[0] - camera.pos[0]) // Defaults.cellHeight]\
+                            [(pygame.mouse.get_pos()[1] - camera.pos[1]) // Defaults.cellWidth] = 0
 
     #MENUBAR CONTROL
     if(pygame.mouse.get_pressed()[0] == True and handled == False):
@@ -126,19 +132,27 @@ while not done:
     if(pygame.mouse.get_pressed()[0] == False):
         handled = False
 
+    #update the game accordingly to the set speed
+    if(isRunning):
+        if(simulationSpeed - passedTime <= 0):
+            grid.applyRules()
+            passedTime = 0
 
-    # --- Screen-clearing code goes here
+    #Clear the screen
     screen.fill(Defaults.WHITE)
  
 
     # --- Drawing code should go here
+    #Draw the grid
     for cX, x in enumerate(grid.grid):
         for cY, y in enumerate(x):
             if not y:
-                DrawUtil.drawRectWithBorder(screen, Defaults.BLACK, Defaults.WHITE, cX * Defaults.cellHeight, cY * Defaults.cellWidth,
+                DrawUtil.drawRectWithBorder(screen, Defaults.BLACK, Defaults.WHITE,
+                                            cX * Defaults.cellHeight + camera.pos[0], cY * Defaults.cellWidth + camera.pos[1],
                                             Defaults.cellHeight, Defaults.cellWidth, 1)
             else:
-                DrawUtil.drawRectWithBorder(screen, Defaults.BLACK, Defaults.BLACK, cX * Defaults.cellHeight, cY * Defaults.cellWidth,
+                DrawUtil.drawRectWithBorder(screen, Defaults.BLACK, Defaults.BLACK,
+                                            cX * Defaults.cellHeight + camera.pos[0], cY * Defaults.cellWidth + camera.pos[1],
                                             Defaults.cellHeight, Defaults.cellWidth, 1)
     #Draw play/stop-button
     DrawUtil.drawRectWithBorder(screen, Defaults.BLACK, Defaults.WHITE, Defaults.stButtonPos[0], Defaults.stButtonPos[1],
@@ -161,9 +175,10 @@ while not done:
                                 Defaults.spUpButtonSize, Defaults.spUpButtonSize, 2)
     DrawUtil.drawRectWithBorder(screen, Defaults.BLACK, Defaults.BLACK, Defaults.spDownButtonPos[0] + 5, Defaults.spDownButtonPos[1] + 15,
                                 30, 10, 2)
-    #draw the reload button
-    DrawUtil.drawRectWithBorder(screen, Defaults.BLACK, Defaults.WHITE, Defaults.restartButtonPos[0], Defaults.spDownButtonPos[1],
-                                Defaults.spUpButtonSize, Defaults.spUpButtonSize, 2)
+    #draw the one-step-button
+    DrawUtil.drawRectWithBorder(screen, Defaults.BLACK, Defaults.WHITE, Defaults.oneStepButtonPos[0], Defaults.oneStepButtonPos[1],
+                                Defaults.oneStepButtonSize, Defaults.oneStepButtonSize, 2)
+    pygame.draw.polygon(screen, Defaults.BLACK, Defaults.oneStepButtonTrianglePoints)
 
 
     # --- Go ahead and update the screen with what we've drawn.
